41f7cec7cc33fef59f478f97891abe2211d1d70d
Eduardo Ono <eduardo.ono@gmail.com>
2020-08-31 23:04:32 -0300

    Programas em C da aula 02 adicionados

---
+++ programas-em-c/aula02b-sizeof.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	int a;
+	
+	printf("Tamanho short = %ld\n", sizeof (short));
+	printf("Tamanho int = %ld\n", sizeof (int));
+	printf("Tamanho long = %ld\n", sizeof (long));	
+		
+	return 0;
+}
+
